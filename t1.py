04f302a6462bf3c927a61d5bf711efc9fd4f7646
Hagar Farouk <63567774+hagarfarouk@users.noreply.github.com>
2022-11-05 21:13:21 +0200

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,24 @@
+
+def normalmode():
+	m = input("enter your equation ")
+	eval(m)
+	print(eval(m))	
+	
+def progmode():
+	n = input("enter your equation ")
+	eval(m)
+	print(eval(m))
+
+
+
+print("Welcome to calculater")
+x = int(input("Enter 1 for Normal Mode \n or 2 for programer mode "))
+if x == 1:
+	normalmode()
+if x == 2:
+	progmode()
+
+
+
+
+				
